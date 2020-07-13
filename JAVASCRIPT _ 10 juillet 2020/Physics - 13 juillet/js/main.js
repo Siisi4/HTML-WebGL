bd79b51288bc3541cf23ff95d2c5c9a8cbbf37a8
Siisi4 <siisi4officiel@gmail.com>
2020-07-13 16:26:28 +0200

    fin de journee

--- JAVASCRIPT _ 10 juillet 2020/Physics - 13 juillet/js/main.js
+++ JAVASCRIPT _ 10 juillet 2020/Physics - 13 juillet/js/main.js
@@ -14,10 +14,22 @@ var ressources = {
 var frame = 0;
 var lastFrameWithEnemy;
 var randomPosForNextEnemy = 100;
+var difficultLevel = 1;
 
 var player = {
     pos:{x:0, y:0},
-    v:{x:0, y:0}
+    v:{x:0, y:0},
+    animations:{
+        frame:0,
+        spacing:15,
+        width: 136,
+        clips:{
+            idle:[0],
+            walk:[0,1,2],
+            jump:[3,4],
+            walkBack:[5,6]
+        }
+    }
 }
 
 var enemies = [
@@ -94,7 +106,7 @@ function imageLoader(url, callback) {
 }
 
 function preloader() {
-    imageLoader("./img/personnage.png", function(image){
+    imageLoader("./img/pc.png", function(image){
         ressources.personnage = image
     })
     imageLoader("./img/enemy.png", function(image){
@@ -107,29 +119,50 @@ function preloader() {
 
 function manageEnemies() {
     if (enemies.length == 0){
-        enemies.push({pos:{x:1200, y:0}, v:3})
+        enemies.push({pos:{x:1200, y:0}, v:2+difficultLevel})
     }else{
         var lastEnemyPos = enemies[enemies.length-1].pos.x
 
         if (lastEnemyPos < (300 + randomPosForNextEnemy) ){
-            enemies.push({pos:{x:1200, y:0}, v:3+Math.random()})
+            enemies.push({pos:{x:1200, y:0}, v:2+difficultLevel+Math.random()})
+            difficultLevel += 0.1
+            if (difficultLevel >= 4){
+                difficultLevel = 1
+            } 
             randomPosForNextEnemy = Math.random()*1000
         }
     }
 
-
     for (var i=0; i< enemies.length;i++){
-        enemies[i].pos.x -= 3
+        enemies[i].pos.x -= enemies[i].v
     }
 }
+
+function manageStates(){
+    var currentState = "idle"
+    if(player.pos.y < 0){
+        currentState = "jump"
+    }else if (player.v.x == 0){
+        currentState = "idle"
+    }else if (player.v.x > 0){
+        currentState = "walk"
+    }else if (player.v.x < 0){
+        currentState = "walkBack"
+    }
+    var clip = player.animations.clips[currentState]
+    var effectiveFrame = player.animations.frame += 1 
+    return effectiveFrame;
+}
+
 
 function render(){
     if(ressources.personnage && ressources.enemy) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(ressources.bg, 0, 0 );
+        manageStates()
         //ctx.fillRect(player.pos.x, player.pos.y+200, 150, 100);
-        ctx.drawImage(ressources.personnage, player.pos.x+10, player.pos.y+400 );
-
+        //ctx.drawImage(ressources.personnage, player.pos.x+10, player.pos.y+370 );
+        ctx.drawImage(ressources.personnage, 0, 0, 102, 168, player.pos.x, player.pos.y+370, 102, 168);
         ctx.drawImage(ressources.enemy, enemies[0].pos.x, enemies[0].pos.y+400 );
         for (var i = 0;i< enemies.length; i++){
             ctx.drawImage(ressources.enemy, enemies[i].pos.x, enemies[i].pos.y+400 );
@@ -139,11 +172,34 @@ function render(){
     }
 }
 
+function createBoundingBox(pos){
+    return {
+        left:pos.x,
+        right:pos.x + 150,
+        top:pos.y,
+        bottom:pos.y + 120 
+    }
+}
+
+function manageCollisions(){
+    var playerBox = createBoundingBox(player.pos)
+    //ctx.fillRect(playerBox.left, playerBox.top+400, playerBox.right-playerBox.left, playerBox.bottom )
+    for (var i=0; i< enemies.length;i++){
+        var enemyBox = createBoundingBox(enemies[i].pos)
+        //ctx.fillRect(enemyBox.left, enemyBox.top+400, enemyBox.right-enemyBox.left, enemyBox.bottom )
+    if(playerBox.right > enemyBox.left && enemyBox.right > playerBox.left){
+        if (playerBox.bottom > enemyBox.top){
+            alert("boum !")
+        }
+    }
+    }
+}
 
 function play() {
     manageEnemies();
     physics();
     render();
+    manageCollisions();
     //console.log("play");
     requestAnimationFrame(play);
 }
